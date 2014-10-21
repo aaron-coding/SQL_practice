6246421cf82dafa79952d040eb2a3c8cdf6c180c
Aaron Cordovez <aaron.cordovez.m@gmail.com>
2014-10-21 16:29:06 -0700

    add lookup functions

--- question_like.rb
+++ question_like.rb
@@ -18,4 +18,77 @@ class QuestionLike
     SQL
     Reply.new(question_like_hash)
   end
+  
+  def self.likers_for_question_id(question_id)
+    users = QuestionsDatabase.instance.execute(<<-SQL, question_id)
+    SELECT users.id, fname, lname
+    FROM 
+      question_likes
+    JOIN 
+      users
+    ON
+      users.id = question_likes.user_id
+    WHERE
+      question_id = ?
+    SQL
+    users.map {|user| User.new(user) }
+  end
+  
+  def self.liked_questions_for_user_id(user_id)
+    questions = QuestionsDatabase.instance.execute(<<-SQL, user_id)
+    SELECT
+      questions.id, title, body, questions.user_id
+    FROM
+    question_likes
+    JOIN
+    questions ON question_likes.question_id = questions.id
+    WHERE
+    question_likes.user_id = ?
+    SQL
+    questions.map { |hash| Question.new(hash) }
+  end
+  
+  def self.num_likes_for_question_id(question_id)
+    nums = QuestionsDatabase.instance.execute(<<-SQL, question_id)
+    SELECT COUNT(users.id) AS num
+    FROM 
+      question_likes
+    JOIN 
+      users
+    ON
+      users.id = question_likes.user_id
+    WHERE
+      question_id = ?
+    SQL
+    nums.first['num']
+  end
+  
+  def create
+    QuestionsDatabase.instance.execute(<<-SQL, @question_id, @user_id)
+    INSERT INTO question_likes (question_id, user_id)
+    VALUES
+    ( ?, ? )
+    SQL
+    
+    @id = QuestionsDatabase.instance.last_insert_row_id
+  end
+  
+  def self.most_liked_questions(n)
+    questions = QuestionsDatabase.instance.execute(<<-SQL, n)
+      SELECT questions.id, body, title, questions.user_id
+      FROM 
+       questions
+      JOIN 
+       question_likes
+      ON 
+        questions.id = question_id
+      GROUP BY
+        questions.id
+      ORDER BY 
+        COUNT(question_id) DESC
+      LIMIT ?
+    SQL
+    questions.map {|hash| Question.new(hash) }
+  end
+  
 end
